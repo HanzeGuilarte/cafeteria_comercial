d62d83015fc6b27bfa4a464b6e2acefc0d0726af
unknown <hanze93guilarte@gmail.com>
2020-10-19 21:08:33 -0700

    eliminado archios inecesario

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { SideProvider } from "./Context/InitialContext";
